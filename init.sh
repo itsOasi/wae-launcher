f4342c2b389052d47d2cf399ac39f18ac21b9951
itsOasi <ostally001@gmail.com>
2022-09-20 10:01:08 -0700

    updated commands to include sudo

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo updating apt...
-apt update &> /dev/null
+echo "" | sudo apt update &> /dev/null
 echo installing pipenv...
 pip install pipenv &> /dev/null
 echo installing git...  
-echo "Y" | apt install git &> /dev/null
+echo "Y" | sudo apt install git &> /dev/null
 echo cloning WAE repo
 git clone dev https://github.com/itsOasi/portfolio wae
