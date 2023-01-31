afd475dfa46647e6a9eb69d5f35514baa0e66962
carpe diem ad infinitum <34924350+itsOasi@users.noreply.github.com>
2023-01-31 15:32:32 -0700

    Update init.sh
    removed git clone command

--- init.sh
+++ init.sh
@@ -5,6 +5,5 @@ echo "" | sudo apt update &> /dev/null
 echo installing pipenv...
 pip install pipenv &> /dev/null
 echo installing git...  
-echo "Y" | sudo apt install git &> /dev/null
-echo cloning WAE repo
-git clone dev https://github.com/itsOasi/portfolio wae
+echo "Y" | sudo apt install git
+
